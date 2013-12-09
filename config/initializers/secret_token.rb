43270682d6969adb233d4d97620e0da0d5d491f0
Juha Mikkola <sales@floorballpro.com>
2013-12-09 17:03:58 -0500

    Created rails app in postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitbyfriday::Application.config.secret_key_base = 'f124ac56a26c00513188de2d50f52bbfb12a8c13989ada962d0a0c1d84877b7c4cdf5d7a6baa422be9df71fbe3c2f1d25174b7a0870e9cd17fe336018885201d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitbyfriday::Application.config.session_store :cookie_store, key: '_fitbyfriday_session'
