e89bb3b366f9e2a2a255720fa33755e8c50856de
Moin Virani <moin.virani@gmail.com>
2013-12-09 21:51:48 -0500

    sorted workouts in order according to day and updated index and controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,7 +3,11 @@ class WorkoutsController < ApplicationController
   def index
     @workouts = Workout.all.group_by { |w| w.start_time.to_date.beginning_of_week }
     @workouts = @workouts[DateTime.now.beginning_of_week.to_date]
+    
+    #@workouts = @workouts.group_by { |w| w.start_time.to_date.beginning_of_day}
+
     # @workouts = @workouts[DateTime.now.next_week.beginning_of_week.to_date]
+    @workouts.sort!{|t1,t2|t1.start_time <=> t2.start_time}
   end
 
   def show
