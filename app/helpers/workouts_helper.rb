bd8ef5fa5087e0eb7e49c988e0deec9e1e6e6dcf
batesy <gbatesy@gmail.com>
2013-12-10 10:46:07 -0500

    Fixed Workout Helper to display Sunday workouts correctly

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,6 +1,6 @@
 module WorkoutsHelper
   def days
     days = [["Monday",1], ["Tuesday",2], ["Wednesday",3],
-     ["Thursday",4], ["Friday", 5], ["Saturday",6], ["Sunday",7]]
+     ["Thursday",4], ["Friday", 5], ["Saturday",6], ["Sunday",0]]
   end
 end
