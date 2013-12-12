f4ab12e4c148f9ea1b119b35b5be86ca22dce271
batesy <gbatesy@gmail.com>
2013-12-11 21:14:39 -0500

    Added Awaiting Confirmation section to workout page. Doesn't work yet

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,5 +2,6 @@ class Workout < ActiveRecord::Base
   belongs_to :creator, :class_name => "User"
   has_many :user_workouts
   has_many :users, through: :user_workouts
+  has_many :invitations
 end
   
