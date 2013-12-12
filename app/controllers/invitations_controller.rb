eae8a807664b746bc3205217fea8f83c7e8aab4e
batesy <gbatesy@gmail.com>
2013-12-12 16:49:48 -0500

    Sender can cancel a workout invite. Only ender and target can see an invite

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,10 +1,14 @@
 class InvitationsController < ApplicationController
+  # create load workout before filter
   def index
   end
 
   def show
     @workout = Workout.find(params[:workout_id])
     @invitation = Invitation.find(params[:id])
+    if (@invitation.sender != current_user) && (@invitation.target != current_user)
+      redirect_to workouts_path
+    end
   end
 
   def accept
