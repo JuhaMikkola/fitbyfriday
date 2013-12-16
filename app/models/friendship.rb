a963586b26a39387b54185488d714be59680680d
Juha Mikkola <sales@floorballpro.com>
2013-12-16 18:19:47 -0500

    Only friends can be invited to workouts and you cannot add a friend more than once

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,7 @@ class Friendship < ActiveRecord::Base
   belongs_to :sender, class_name: "User", foreign_key: 'sender_id'
   belongs_to :target, class_name: "User", foreign_key: 'target_id'
 
+  # def friends
+    
+
 end
