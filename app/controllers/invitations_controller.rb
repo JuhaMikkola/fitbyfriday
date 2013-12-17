eb24945baae0e71d5174cdd69878da06591dae55
batesy <gbatesy@gmail.com>
2013-12-16 23:54:59 -0500

    Can invite multiple friends to a workout. Ratings working.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -14,17 +14,15 @@ class InvitationsController < ApplicationController
   def accept
     @invitation = Invitation.find(params[:id])
     @workout = Workout.find(params[:workout_id])
-    current_user.workouts << @workout
+    # current_user.workouts << @workout MOVED THIS DOWN AFTER RATINGS ARE CREATED
     @invitation.update_attribute("confirmed", true)
-    @partners = @workout.user_workouts.select { |u| u.user_id != current_user }
-    @partners.each do |p|
+    # @partners = @workout.user_workouts.select { |u| u.user_id != current_user }
+    # @partners.each do |p|
+    @workout.user_workouts.each do |p|
       Rating.create(workout_id: p.workout_id, rater_id: current_user.id, rated_id: p.user_id)
-      
       Rating.create(workout_id: p.workout_id, rater_id: p.user_id, rated_id: current_user.id)
-    
     end
-
-
+    current_user.workouts << @workout
     redirect_to workouts_path
   end
 
