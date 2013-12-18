2fb8ed8c9b09a5603f56f8646f99c06d26ef2627
Moin Virani <moin.virani@gmail.com>
2013-12-17 23:20:30 -0500

    added gravatar image so you can now see users profile pic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  def avatar_url(user)
+    if user.avatar.present?
+      user.avatar
+    else
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=80&d=#{CGI.escape(default_url)}"
+    end
+  end
+
+
 end
