38248a0ce841ab51848b90a596b2595b3597cda0
batesy <gbatesy@gmail.com>
2013-12-20 16:33:35 -0500

    Fix duplicate friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,7 @@ class Friendship < ActiveRecord::Base
   belongs_to :sender, class_name: "User", foreign_key: 'sender_id'
   belongs_to :target, class_name: "User", foreign_key: 'target_id'
 
+  validates [:sender, :target], :uniqueness => true
   # def friends
 
 
